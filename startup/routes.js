c71784fd2fa12332918f585439f1ec89b09ce6e2
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-04-14 02:29:04 +0500

    update

--- startup/routes.js
+++ startup/routes.js
@@ -13,8 +13,8 @@ const coinMarketRouter = require("../routes/coin_market");
 const profileUpdateRouter = require("../routes/user_update");
 const adminNotifyRouter = require("../routes/notifications");
 const adminSettingRouter=require("../routes/admin_settings");
-const adminPackagesRouter=require("../routes/package");
-const packagePurchaseRouter=require("../routes/purchase");
+// const adminPackagesRouter=require("../routes/package");
+// const packagePurchaseRouter=require("../routes/purchase");
 
 
 var corsOptions = {
@@ -43,6 +43,6 @@ module.exports = function (app) {
   app.use("/api/profile/", profileUpdateRouter);
   app.use("/api/admin/notification/", adminNotifyRouter);
   app.use("/api/setting",adminSettingRouter)
-  app.use("/api/package",adminPackagesRouter)
-  app.use("/api/purchase",packagePurchaseRouter)
+  // app.use("/api/package",adminPackagesRouter)
+  // app.use("/api/purchase",packagePurchaseRouter)
 };
