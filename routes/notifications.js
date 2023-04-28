174f754ab6e2696c66d06b356bd725aad73d53b8
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-04-29 01:07:16 +0500

    update

--- routes/notifications.js
+++ routes/notifications.js
@@ -106,6 +106,36 @@ router.put("/hide/:id", async (req, res) => {
   }
 });
 
+router.put("/show/:id", async (req, res) => {
+  try {
+    if (!req.params.id)
+      return res.status(400).send("Please Provide Id To Update.");
+    if(!req.body.user_id)
+      return res.status(400).send("User Id is required");
+    const checkNotification = await Notifications.findAll({where:{id:req.params.id}});
+    if (!checkNotification.length > 0)
+      return res.status(404).send("Notifications Not Found.");
+
+    checkNotification.forEach((noti) => {
+      if (noti.hide) {
+        let st = noti.hide.split(",");
+        let index = st.indexOf(`${req.body.user_id}`);
+        if (index !== -1) {
+          st.splice(index, 1);
+        }
+        noti.hide = st.toString();
+        noti.save();
+      }
+    });
+
+    req.io.emit("newNotification",{message:"shown"});
+    return res.send("shown.");
+  } catch (error) {
+    return res.send(error.message);
+  }
+})
+
+
 router.put("/:id", async (req, res) => {
   try {
     if (!req.params.id)
