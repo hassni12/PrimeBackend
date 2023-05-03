115232e4b605c7319fae449ab899ded2c412ba85
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-05-04 01:37:45 +0500

    update

--- routes/coin_market.js
+++ routes/coin_market.js
@@ -3,7 +3,7 @@ const CoinMarket = require("../models/coin_market");
 const router = express.Router();
 router.get("/", async (req, res) => {
   try {
-    const coins = await CoinMarket.findAll({where:{allow:true}});
+    const coins = await CoinMarket.findAll();
 
     res.status(200).send(coins);
   } catch (error) {
