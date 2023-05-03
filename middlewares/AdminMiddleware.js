560d2f69ec6d4ad5273fff680a925b0d1927dc3d
sulaiman-s <sulaimanfarooqi526@gmail.com>
2023-05-04 01:54:08 +0500

    update

--- middlewares/AdminMiddleware.js
+++ middlewares/AdminMiddleware.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   if (!req.header("x-auth-token"))
-    return res.status(404).send("authentication token required");
+    return res.status(401).send("authentication token required");
   try {
     const decodetoken = jwt.verify(req.header("x-auth-token"), "privateKey");
     req.user = decodetoken;
     if (decodetoken.is_admin) {
       next();
     } else {
-      return res.status(400).send("Invalid Token.");
+      return res.status(401).send("Invalid Token.");
     }
   } catch (error) {
-    return res.status(400).send("Invalid token");
+    return res.status(401).send("Invalid token");
   }
 };
--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   if (!req.header("x-auth-token"))
-    return res.status(400).send("authentication token required");
+    return res.status(401).send("authentication token required");
   try {
     const decodetoken = jwt.verify(req.header("x-auth-token"), "privateKey");
     req.user = decodetoken;
@@ -9,9 +9,9 @@ module.exports = function (req, res, next) {
     if (decodetoken.is_admin || decodetoken.is_active) {
       next();
     } else {
-      return res.status(400).send("Invalid Token");
+      return res.status(401).send("Invalid Token");
     }
   } catch (error) {
-    return res.status(400).send("invalid token");
+    return res.status(401).send("invalid token");
   }
 };
